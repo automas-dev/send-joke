a4290a363c5298ed701f931c3a797c9aa9fed4a5
Thomas Harrison <twh2898@vt.edu>
2019-04-30 17:09:32 -0400

    Added support for unicode characters in message subject and body

--- send_joke.py
+++ send_joke.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
 import ssl
 import requests
 import datetime
@@ -16,11 +19,17 @@ def get_joke(cfg):
 
 def send_mail(srv, mail, message):
     context = ssl.create_default_context()
+
     with smtplib.SMTP_SSL(srv['address'], srv['port'], context=context) as server:
         server.login(srv['user'], srv['password'])
+
+        msg = MIMEText(message, 'html', 'utf8')
+        msg['from'] = mail['email']
+        msg['subject'] = Header(mail["subject"], 'utf8')
+        
         for to in mail['to']:
-            subject = f'Subject: {cfg["mail"]["subject"]}\n\n'
-            server.sendmail(mail['email'], to, subject + message)
+            msg['to'] = to
+            server.sendmail(msg['from'], to, msg.as_string())
 
 if __name__ == '__main__':
     cfg = load_config()
